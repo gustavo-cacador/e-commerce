380afc521d9020a45fb2b0f00de9b15734a5bc27
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2025-02-02 20:54:45 -0300

    entidade Pedido

---
+++ src/main/java/br/com/gustavo/ecommerce/entities/Pedido.java
@@ -0,0 +1,25 @@
+package br.com.gustavo.ecommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_pedido")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant momento; // momento em que o pedido foi feito
+
+    private OrderStatus status;
+
+
+    // Muitos Pedidos para um Usuário
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Usuario cliente;
+}
