b0d695b1f0c459847811c7fb4749ccb8b5cc994a
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2025-02-02 23:21:56 -0300

    adicionando na entidade pedido

--- src/main/java/br/com/gustavo/ecommerce/entities/Pedido.java
+++ src/main/java/br/com/gustavo/ecommerce/entities/Pedido.java
@@ -22,4 +22,58 @@ public class Pedido {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private Usuario cliente;
+
+    @OneToOne(mappedBy = "pedido", cascade = CascadeType.ALL)
+    private Pagamento pagamento;
+
+    public Pedido() {
+    }
+
+    public Pedido(Long id, Instant momento, PedidoStatus status, Usuario cliente, Pagamento pagamento) {
+        this.id = id;
+        this.momento = momento;
+        this.status = status;
+        this.cliente = cliente;
+        this.pagamento = pagamento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMomento() {
+        return momento;
+    }
+
+    public void setMomento(Instant momento) {
+        this.momento = momento;
+    }
+
+    public PedidoStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PedidoStatus status) {
+        this.status = status;
+    }
+
+    public Usuario getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Usuario cliente) {
+        this.cliente = cliente;
+    }
+
+    public Pagamento getPagamento() {
+        return pagamento;
+    }
+
+    public void setPagamento(Pagamento pagamento) {
+        this.pagamento = pagamento;
+    }
 }
