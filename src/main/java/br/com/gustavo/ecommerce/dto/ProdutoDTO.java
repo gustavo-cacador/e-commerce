1e19654398952450a2df11b765effc587821dd69
Gustavo Caçador <gustavoirl456@outlook.com>
2025-02-05 15:59:34 -0300

    produto DTO

---
+++ src/main/java/br/com/gustavo/ecommerce/dto/ProdutoDTO.java
@@ -0,0 +1,52 @@
+package br.com.gustavo.ecommerce.dto;
+
+import br.com.gustavo.ecommerce.entities.Produto;
+import jakarta.persistence.Column;
+
+public class ProdutoDTO {
+
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Double preco;
+    private String imgUrl;
+
+    public ProdutoDTO() {
+    }
+
+    public ProdutoDTO(Long id, String nome, String descricao, Double preco, String imgUrl) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.imgUrl = imgUrl;
+    }
+
+    public ProdutoDTO(Produto entity) {
+        id = entity.getId();
+        nome = entity.getNome();
+        descricao = entity.getDescricao();
+        preco = entity.getPreco();
+        imgUrl = entity.getImgUrl();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+}
