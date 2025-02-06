8864f23a8aeeb9c7ded1a5ab74c53c0bbb569f87
Gustavo Caçador <gustavoirl456@outlook.com>
2025-02-06 16:52:50 -0300

    excecao customizada

--- src/main/java/br/com/gustavo/ecommerce/services/ProdutoService.java
+++ src/main/java/br/com/gustavo/ecommerce/services/ProdutoService.java
@@ -3,12 +3,15 @@ package br.com.gustavo.ecommerce.services;
 import br.com.gustavo.ecommerce.dto.ProdutoDTO;
 import br.com.gustavo.ecommerce.entities.Produto;
 import br.com.gustavo.ecommerce.repositories.ProdutoRepository;
+import br.com.gustavo.ecommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.swing.text.html.parser.Entity;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,7 +23,8 @@ public class ProdutoService {
 
     @Transactional(readOnly = true)
     public ProdutoDTO findById(Long id) {
-        Produto produto = produtoRepository.findById(id).get();
+        Produto produto = produtoRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Produto com id: " + id + ", não encontrado."));
         return new ProdutoDTO(produto);
     }
 
@@ -52,10 +56,15 @@ public class ProdutoService {
 
     @Transactional
     public ProdutoDTO update(Long id, ProdutoDTO dto) {
-        Produto entity = produtoRepository.getReferenceById(id);
-        copyDtoToEntity(dto, entity);
-        entity = produtoRepository.save(entity);
-        return new ProdutoDTO(entity);
+        try {
+            Produto entity = produtoRepository.getReferenceById(id);
+            copyDtoToEntity(dto, entity);
+            entity = produtoRepository.save(entity);
+            return new ProdutoDTO(entity);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Produto com id: " + id + ", não encontrado.");
+        }
     }
 
     @Transactional
