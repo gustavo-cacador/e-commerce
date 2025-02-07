9e5cde2ed9ef49fbcd7e62acca651a2d0060ddfe
Gustavo Caçador <gustavoirl456@outlook.com>
2025-02-07 16:17:38 -0300

    exception handler delete

--- src/main/java/br/com/gustavo/ecommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/br/com/gustavo/ecommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.com.gustavo.ecommerce.controllers.handlers;
 
 import br.com.gustavo.ecommerce.dto.CustomError;
+import br.com.gustavo.ecommerce.services.exceptions.DatabaseException;
 import br.com.gustavo.ecommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -16,6 +17,13 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
