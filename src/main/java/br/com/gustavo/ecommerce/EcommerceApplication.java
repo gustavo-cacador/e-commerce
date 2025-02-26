e13f24e9d856a0e3d0138900a5c7afca72fc6efc
Gustavo Caçador <gustavoirl456@outlook.com>
2025-02-26 15:40:03 -0300

    BCrypt password

--- src/main/java/br/com/gustavo/ecommerce/EcommerceApplication.java
+++ src/main/java/br/com/gustavo/ecommerce/EcommerceApplication.java
@@ -1,13 +1,27 @@
 package br.com.gustavo.ecommerce;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class EcommerceApplication {
+public class EcommerceApplication implements CommandLineRunner {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("ENCODE " + passwordEncoder.encode("123456") );
+
+		// como o spring security verifica as credenciais quando o usuario for logar no sistema
+		boolean result = passwordEncoder.matches("123456", "$2a$10$K1odOK387dhqXRx3kOyDDeKu4CZ1y5KYPcSF1vkHf26mjQixR7hpG");
+		System.out.println("Resultado = " + result);
+	}
 }
--- src/main/java/br/com/gustavo/ecommerce/config/SecurityConfig.java
+++ src/main/java/br/com/gustavo/ecommerce/config/SecurityConfig.java
@@ -3,10 +3,18 @@ package br.com.gustavo.ecommerce.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    // quando o usuario inserir a senha, a senha n vai ser registrada exatamente no banco (ele cria um hash derivado da String senha)
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
