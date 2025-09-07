22892b50635b2657aa86370bbf178acdaa76bac8
Gustavo <gustavoirl456@outlook.com>
2025-09-07 16:41:14 -0300

    teste de integração usando REST Assured buscando produto por nome

--- ecommerce-restassured/src/test/java/br/com/gustavo/ecommerce/controllers/ProductControllerRA.java
+++ ecommerce-restassured/src/test/java/br/com/gustavo/ecommerce/controllers/ProductControllerRA.java
@@ -10,10 +10,13 @@ import static org.hamcrest.Matchers.*;
 public class ProductControllerRA {
 
     private Long existingProductId, nonExistingProductId;
+    private String productName;
 
     @BeforeEach
     void setUp() {
         baseURI = "http://localhost:8080";
+
+        productName = "Macbook";
     }
 
     @Test
@@ -21,7 +24,8 @@ public class ProductControllerRA {
 
         existingProductId = 2L;
 
-        given().get("products/{id}", existingProductId)
+        given()
+                .get("products/{id}", existingProductId)
                 .then()
                 .statusCode(200)
                 .body("id", is(2))
@@ -30,5 +34,41 @@ public class ProductControllerRA {
                 .body("price", is(2190.0F))
                 .body("categories.id", hasItems(2,3))
                 .body("categories.name", hasItems("Eletrônicos", "Computadores"));
+    }
+
+    // busca paginada existe listagem paginada qnd campo nome nao preenchido e checa se os produtos Macbook Pro e PC Gamer Tera estao contidos
+    @Test
+    public void findAllShouldReturnPageProductsWhenProductNameIsEmpty() {
+
+        given()
+                .get("products?page=0")
+                .then()
+                .statusCode(200)
+                .body("content.name", hasItems("Macbook Pro", "PC Gamer Tera"));
+    }
+
+    // busca paginada filtra produtos por nome e exibe listagem paginada qnd campo nome preenchido
+    @Test
+    public void findAllShouldReturnPageProductsWhenProductNameIsNotEmpty() {
+
+        given()
+                .get("products?name={productName}", productName)
+                .then()
+                .statusCode(200)
+                .body("content.id[0]", is(3))
+                .body("content.name[0]", equalTo("Macbook Pro"))
+                .body("content.price[0]", is(1250.0F))
+                .body("content.imgUrl[0]", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
+    }
+
+    // busca paginada filtra produtos de forma paginada e filtra produtos com preço maior que 2000
+    @Test
+    public void findAllShouldReturnPagedProductsWithPriceGreaterThen2000() {
+
+        given()
+                .get("products?size=25")
+                .then()
+                .statusCode(200)
+                .body("content.findAll { it.price > 2000 }.name", hasItems("Smart TV", "PC Gamer Hera"));
     }
 }
